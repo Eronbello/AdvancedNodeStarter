495f9479a96fd1732aa42f69ca56309e9f61a9b5
Eronbello <eron.bello@gmail.com>
2020-06-27 15:13:17 -0300

    update: dev keys

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '559824693295-k5ntfrqe4okdohaeahlfflqeaoq1qgq1.apps.googleusercontent.com',
+  googleClientSecret: 'm54b8jKd8rdSJZj9EcuR9gIE',
+  mongoURI: 'mongodb://eron:Sk4o51LDtoReWuYg@cluster0-shard-00-00.kgzkr.mongodb.net:27017,cluster0-shard-00-01.kgzkr.mongodb.net:27017,cluster0-shard-00-02.kgzkr.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-m0etso-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
